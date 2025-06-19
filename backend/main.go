e38f2b57fbe60efc2c459c5b19db9a819ac9165d
DonShanilka <shanilka799@gmail.com>
2025-06-19 13:57:32 +0530

    Update module path in go.mod and remove unused imports in main.go

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"./config"
-	"./routes"
+	"fmt"
 )
 
 func main() {
-	config.ConnectDB()
-	routes.RegisterRoutes()
-
-	log.Println("Server started on http://localhost:3001")
-	log.Fatal(http.ListenAndServe(":3001", nil))
+	
 }
