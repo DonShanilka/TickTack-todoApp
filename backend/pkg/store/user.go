47cc31e2c21ccad3ca74e38b28c0ce30eb4bba20
DonShanilka <shanilka799@gmail.com>
2025-06-21 10:43:05 +0530

    Add database connection variable and update saveUser function to use it

--- backend/pkg/store/user.go
+++ backend/pkg/store/user.go
@@ -2,7 +2,10 @@ package store
 
 import (
 	"log"
+	"database/sql"
 )
+
+var DB *sql.DB
 
 type User struct {
 	ID int
@@ -11,5 +14,11 @@ type User struct {
 }
 
 func saveUser(user User) error {
-
+	_, err := DB.Exec("INSERT INTO users (id, username, password) VALUES (?, ?, ?)", user.ID, user.serName, user.password)
+	if err != nil {
+		log.Printf("Error saving user: %v", err)
+		return err
+	}
+	log.Printf("User %s saved successfully", user.serName)
+	return nil
 }
