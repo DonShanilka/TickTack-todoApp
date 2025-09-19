891d10b1f4158ee1b3df76346788daa26dc62b04
DonShanilka <shanilka799@gmail.com>
2025-09-19 22:18:53 +0530

    feat: enhance CreateListHandler to validate request fields and handle list creation

--- backend/internal/handler/listHandler.go
+++ backend/internal/handler/listHandler.go
@@ -17,4 +17,20 @@ func CreateListHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
+	var req ListRequest
+	
+	if req.Title == "" || req.UserID == 0 || req.UserEmail == "" {
+		http.Error(w, "Titel, UserID and UserEmail are required", http.StatusBadRequest)
+		return
+	}
+
+	list := store.List{Title: req.Title, UserID: req.UserID, UserEmail: req.UserEmail}
+	if err := store.SaveList(list); err != nil {
+		http.Error(w, "Error saving list", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "List created successfully"})
 }
