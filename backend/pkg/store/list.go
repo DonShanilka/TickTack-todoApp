709a5f282723ae2d6100870e0b4bf5642cd7c3cf
DonShanilka <shanilka799@gmail.com>
2025-09-19 14:10:03 +0530

    feat: add initial List struct and database connection setup

---
+++ backend/pkg/store/list.go
@@ -0,0 +1,17 @@
+package store
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+type List struct {
+	ID        int
+	Title     string
+	UserID    int
+	UserEmail string
+}
