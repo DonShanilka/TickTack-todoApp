d4c62185801dddb9a2feb3653a891d8225336896
DonShanilka <shanilka799@gmail.com>
2025-09-19 13:53:28 +0530

    fix: correct UserID field naming in ListRequest struct

--- backend/internal/handler/listHandler.go
+++ backend/internal/handler/listHandler.go
@@ -7,6 +7,7 @@ import (
 )
 
 type ListRequest struct {
-	Title  string `json:"title"`
-	UserID int    `json:"user_id"`
+	Title     string `json:"title"`
+	UserID    int    `json:"userId"`
+	UserEmail string `json:"userEmail"`
 }
